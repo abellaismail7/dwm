ed98dda483d2f50580a09232708292ade182b33c
Ismail A Bella <abellaismail@gmail.com>
2020-12-19 13:29:04 +0100

    color & alpha tweaks

--- config.def.h
+++ config.def.h
@@ -17,17 +17,17 @@ static const char *fonts[]          = {
 };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#292f36";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#e0fbfc";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 0xd0;
+static const char col_cyan[]        = "#fcba03";
+static const unsigned int baralpha = 0xf0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
